aa31360fb09df57c31c2219a420f81806dc2802d
Per Ivar Nerseth <perivar@nerseth.com>
2017-03-31 22:12:35 +0200

    Added SASS css generation

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
 	cleanCSS = require('gulp-clean-css'),
+	autoprefixer = require('gulp-autoprefixer'),
+	sass = require('gulp-sass'),
 	rename = require('gulp-rename'),
     util = require('gulp-util'),
     imagemin = require('gulp-imagemin'),
@@ -8,7 +10,7 @@ var gulp = require('gulp'),
 	htmlmin  = require('gulp-htmlmin'),
 	htmlreplace = require('gulp-html-replace');
 	
-gulp.task('build', ['minify-js', 'minify-css', 'minify-image', 'html-replace', 'copy-static']);
+gulp.task('build', ['minify-js', 'sass', 'minify-image', 'html-replace', 'copy-static']);
 
 gulp.task('minify-js', function() {
   gulp.src(['src/js/quiz.js'])
@@ -27,15 +29,19 @@ gulp.task('minify-css', function () {
   .pipe(rename({
     extname: '.min.css'
   }))
-  .pipe(gulp.dest('build/css'));
-  
-  gulp.src('src/css/style.css') 
-  .pipe(cleanCSS({compatibility: 'ie8'}))
+  .pipe(gulp.dest('build/css'));  
+});
+
+gulp.task('sass', function() {
+  gulp.src(['src/scss/**/*.scss'])
+  .pipe(sass())
   .on('error', util.log)
+  .pipe(autoprefixer())
+  .pipe(cleanCSS({compatibility: 'ie8'}))
   .pipe(rename({
     extname: '.min.css'
   }))
-  .pipe(gulp.dest('build/css'));  
+  .pipe(gulp.dest('build/css/'));
 });
 
 gulp.task('minify-image', function () {
@@ -44,32 +50,15 @@ gulp.task('minify-image', function () {
   .pipe(gulp.dest('build/images'));
 });
 
-gulp.task('html', function() {
-  gulp.src('src/index.html')
-  .pipe(htmlreplace({
-   'css': 'css/quiz.min.css',
-   'js': 'js/quiz.min.js'
-  }))
-  .pipe(htmlmin({
-	  collapseWhitespace: false, 
-	  minifyJS: true,
-	  removeRedundantAttributes: true,
-      removeScriptTypeAttributes: true,
-      removeStyleLinkTypeAttributes: true
-	  }))
-  .pipe(gulp.dest('build/'));
-});
-
 gulp.task('html-replace', function() {
   gulp.src('src/index.html')
   .pipe(replace(/quiz\.css/g, 'quiz.min.css'))
-  .pipe(replace(/style\.css/g, 'style.min.css'))
   .pipe(replace(/quiz\.js/g, 'quiz.min.js'))
   .pipe(replace(/images\//g, 'http://server.wazalo.com/WazaloQuizClient/images/')) 
   .pipe(htmlmin({
 	  collapseWhitespace: true, 
 	  minifyJS: true,
-	  removeRedundantAttributes: true,
+	  //removeRedundantAttributes: true,
       removeScriptTypeAttributes: true,
       removeStyleLinkTypeAttributes: true
 	  }))
@@ -81,7 +70,7 @@ gulp.task('copy-static', function() {
   gulp.src(['src/js/**/*.js', '!src/js/**/quiz.js'])
   .pipe(gulp.dest('build/js'));
 
-  gulp.src(['src/css/**/*.css', '!src/css/**/quiz.css', '!src/css/**/style.css'])
+  gulp.src(['src/css/**/*.css', '!src/css/**/quiz.css'])
   .pipe(gulp.dest('build/css'));
 });
 		
